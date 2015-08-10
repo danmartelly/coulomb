dc3d4baf364d8618729766b25c63b820ac949b46
Stephanie Gu <steph.gu@gmail.com>
2015-08-09 17:01:06 -0700

    added bucket game

---
+++ Assets/Scripts/bucketGoal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bucketGoal : MonoBehaviour {
+	public TextMesh winText;
+
+	void Start()
+	{
+		winText.text = "";
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+
+			winText.text = "you win!";
+		}
+	}
+}
---
+++ Assets/Scripts/wasdMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wasdMove : MonoBehaviour {
+
+	float speed = 5.0f; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w")) {
+			transform.Translate (new Vector3(0, 0, 1) * Time.deltaTime * speed, Space.World);
+		}
+		if (Input.GetKey ("s")) {
+			transform.Translate (new Vector3(0, 0, -1) * Time.deltaTime * speed, Space.World);
+		}
+		if (Input.GetKey ("a")) {
+			transform.Translate (new Vector3(-1, 0, 0) * Time.deltaTime * speed, Space.World);
+		}
+		if (Input.GetKey ("d")) {
+			transform.Translate (new Vector3(1, 0, 0) * Time.deltaTime * speed, Space.World);
+		}
+	}
+}
